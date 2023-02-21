476be212a24c22590688577cc6d53ed854219b5c
Andrey Bolshakov <abolshakovy@gmail.com>
2023-02-21 23:21:20 +0500

    feature(core) Main features done

---
+++ data_from_api.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+# https://docs.google.com/spreadsheets/d/1RpHXQgM1_1zIrLdbjM7HxpgfLNvDBWrqTZSeHd7CikA/edit?usp=sharing
+SAMPLE_SPREADSHEET_ID = '1RpHXQgM1_1zIrLdbjM7HxpgfLNvDBWrqTZSeHd7CikA'
+SAMPLE_RANGE_NAME = 'Data!A1:D10'
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        # Запишем данные в csv файл для дальнейшей обработки
+        with open('datasource.txt', 'w') as f:
+            # save results as dictionare in csv file
+            for row in result['values']:
+                # create dictionary
+                dictionary = {'Author': row[0], 'review text': row[1], 'Date': row[2]}
+                # write dictionary to csv file
+                f.write(f"{dictionary['Author']}/{dictionary['review text']}/{dictionary['Date']}/")
+                f.write('\n')
+            f.close()
+    except HttpError as err:
+        print("API import error", err)
--- main.py
+++ main.py
@@ -1,48 +1,90 @@
 import csv
+import os
+from collections import defaultdict
 
-FILE_PATH = "https://docs.google.com/spreadsheets/d/1RpHXQgM1_1zIrLdbjM7HxpgfLNvDBWrqTZSeHd7CikA/edit?usp=sharing"
+from dotenv import load_dotenv
+
+import openai
+
+import data_from_api
+
+load_dotenv()
+
+FILE_PATH = "datasource.txt"
 FILE_OTPUT_PATH = "/Users/andreybolshakov/PycharmProjects/Test_tasks/Welltory/file_analyzed.csv"
 
 
-def read_csv_file(filepath):
-    with open(filepath, 'r') as f:
-        reader = csv.DictReader(f)
-        return [row for row in reader]
-
-
-def analyze_reviews(reviews):
-    for review in reviews:
-        text = review['review text']
-        # Perform analysis of the text
-        # Set the rating in the "Rate" field
-        review['Rate'] = 5  # Replace this with the actual rating determined from the analysis
-
-    # Sort the reviews by rating in descending order
-    reviews.sort(key=lambda x: x['Rate'], reverse=True)
+# read txt fiel and return list of dictionaries
+def read_txt_file(FILE_PATH):
+    with open(FILE_PATH, 'r') as f:
+        # read file as list of strings
+        lines = f.readlines()
+        # create list of dictionaries
+        reviews = []
+        for line in lines[1:]:
+            # create dictionary
+            dictionary = {'Author': line.split('/')[0], 'review text': line.split('/')[1], 'Date': line.split('/')[2]}
+            # add dictionary to list
+            reviews.append(dictionary)
     return reviews
 
 
-def write_analyzed_reviews_to_csv(file_path, reviews):
-    file_name = file_path.split('/')[-1].split('.')[0]
-    new_file_name = f"{file_name}_analyzed.csv"
-    with open(new_file_name, 'w', newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=reviews[0].keys())
-        writer.writeheader()
-        for review in reviews:
-            writer.writerow(review)
+def analyze_reviews(reviews):
+    """
+    Analyzes the sentiment of each review using OpenAI's GPT-3 API and returns a dictionary mapping each author's email
+    address to their review's rating on a scale from 1 to 10.
+    """
+    request = '''
+Rate the assessment emotional of user reviews on a scale of 1 to 10,
+where 10 is the most positive and 1 is the most negative.
+Try not to give the same ratings.
+Output only the rating number. | format:plain
+    '''
+    # Load OpenAI API key from environment variable
+    openai.api_key = os.environ['OPENAI_API_KEY']
+
+    # Create a defaultdict to store each author's review and its rating
+    author_ratings = defaultdict(int)
+
+    # Loop over all reviews and analyze their sentiment using OpenAI's GPT-3 API
+    for review in reviews:
+        author = review['Author']
+        text = review['review text']
+        prompt = f"{request}\n{text}"
+        response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=64)
+        rating = response.choices[0].text.strip()
+        # author_ratings[author] = int(rating)
+        # check if rating is not digitized, find in sthe string only digits and convert to int
+        if not rating.isdigit():
+            rating = ''.join(filter(str.isdigit, rating))
+            author_ratings[author] = int(rating)
+        else:
+            author_ratings[author] = int(rating)
+
+    return author_ratings
 
 
-def generate_email_list(reviews):
+def generate_email_list(reviews, analyzed_reviews):
     email_list = []
     for review in reviews:
-        email_list.append(f"{review['Author']} - rate: {review['Rate']}")
+        email_list.append({'Author': review['Author'], 'rate': analyzed_reviews[review['Author']]})
+        email_list.sort(key=lambda x: x['rate'], reverse=True)
     return email_list
 
 
+def write_to_csv(file_path, email_list):
+    file_name = file_path.split('/')[-1].split('.')[0]
+    new_file_name = f"{file_name}_analyzed.csv"
+    with open(new_file_name, 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=email_list[0].keys())
+        writer.writeheader()
+        for email in email_list:
+            writer.writerow(email)
+
+
 if __name__ == '__main__':
-    file_path = "path/to/file.csv"
-    reviews = read_csv_file(FILE_PATH)
-    analyzed_reviews = analyze_reviews(reviews)
-    write_analyzed_reviews_to_csv(FILE_PATH, analyzed_reviews)
-    email_list = generate_email_list(analyzed_reviews)
-    print(email_list)
+    data_from_api.main()
+    data = read_txt_file(FILE_PATH)
+    analyzed_reviews = analyze_reviews(data)
+    email_list = generate_email_list(data, analyzed_reviews)
+    write_to_csv(FILE_PATH, email_list)
